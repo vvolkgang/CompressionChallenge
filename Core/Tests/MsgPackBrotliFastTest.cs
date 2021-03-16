eac7b62be945ef015aa238ccd038bfe67e63c13c
Alison Fernandes <alison.fernandes@xpand-it.com>
2021-03-16 20:15:17 +0000

    Added tests using Brotli slow / fast, updated README

---
+++ Core/Tests/MsgPackBrotliFastTest.cs
@@ -0,0 +1,33 @@
+﻿using Core.Data;
+using MessagePack;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+namespace Core.Tests
+{
+    public class MsgPackBrotliFastTest : BaseTest
+    {
+        public override string TestName => "MsgPack + Brotli Fast";
+
+        public override bool IsBaseline => false;
+
+        public override string Filename => "test.msgpackbr";
+
+        public override byte[] Execute(List<Contact> list)
+        {
+            var pack = MessagePackSerializer.Serialize(list);
+
+            var ms = new MemoryStream();
+            using (var source = new MemoryStream(pack))
+            using (var compressorStream = new BrotliStream(ms, CompressionLevel.Fastest)) 
+            {
+                source.CopyTo(compressorStream);
+            }
+            var bytes = ms.ToArray();
+            ms.Dispose();
+
+            return bytes;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -24,15 +24,16 @@ Using JSON string as a baseline, the following format / compression combos where
 
 * BSON (no compression)
 * JSON + LZ4
-* JSON + Gzip
+* JSON + Gzip Fast and Slow
 * MsgPack (no compression)
 * MsgPack + LZ4
 * MsgPack + LZ4Array
-* MsgPack + GZip
+* MsgPack + GZip Fast and Slow
+* MsgPack + Brotli Fast and Slow (Brotli slow is _*slow*_, like 1.5s slow in my machine, disabled it by default)
 
 # Results
 
-![Results](/docs/results_v2.png)
+![Results](/docs/results_v3.png)
 
 # How to add a new test
 
@@ -49,3 +50,5 @@ Just copy `JsonTest.cs` and add your own implementation to it.
 * [Newtonsoft.Json](https://github.com/JamesNK/Newtonsoft.Json)
 * [ByteSize](https://github.com/omar/ByteSize)
     * Currently not supporting .net5, added the files to the project directly
+
+
