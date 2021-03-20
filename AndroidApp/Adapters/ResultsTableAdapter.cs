0eced8d97f1e7e2f89de01b28aadc8dce2c4f51f
Alison Fernandes <alison.fernandes@xpand-it.com>
2021-03-20 22:59:15 +0000

    First draft of SortableTableView

---
+++ AndroidApp/Adapters/ResultsTableAdapter.cs
@@ -0,0 +1,172 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ByteSizeLib;
+using CodeCrafters.TableViews;
+using Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AndroidApp.Adapters
+{
+    public class ResultViewModel : Java.Lang.Object
+    {
+        private ByteSize _size;
+        private float _gainPerc;
+        private float _executionTimeInMs;
+
+        public ResultViewModel(TestResult result)
+        {
+            Method = result.Method;
+            _size = result.Size;
+            _gainPerc = result.GainPerc;
+            _executionTimeInMs = result.ExecutionTimeInMs;
+        }
+
+        public string Method { get; private set; }
+
+        public string Bytes => _size.Bytes.ToString();
+
+        public string KBytes => _size.KiloBytes.ToString("F0");
+
+        public string SizeDiff => GainToString(_gainPerc);
+
+        public string TimeAvg => _executionTimeInMs.ToString();
+
+        string GainToString(double gainPerc) => gainPerc switch
+        {
+            0 => "-",
+            _ => gainPerc.ToString("P")
+        };
+
+        public static List<ResultViewModel> From(List<TestResult> result)
+        {
+            return result.Select(x => new ResultViewModel(x)).ToList();
+        }
+
+        public static Java.Util.IComparator GetBytesComparator() => new BytesComparator();
+        
+        public static Java.Util.IComparator GetTimeComparator() => new TimeComparator();
+
+        private class BytesComparator : Java.Lang.Object, Java.Util.IComparator
+        {
+            public int Compare(Java.Lang.Object lhs, Java.Lang.Object rhs)
+            {
+                var r1 = (ResultViewModel)lhs;
+                var r2 = (ResultViewModel)rhs;
+                return r1._size.CompareTo(r2._size);
+            }
+        }
+
+        private class TimeComparator : Java.Lang.Object, Java.Util.IComparator
+        {
+            public int Compare(Java.Lang.Object lhs, Java.Lang.Object rhs)
+            {
+                var r1 = (ResultViewModel)lhs;
+                var r2 = (ResultViewModel)rhs;
+                return r1._executionTimeInMs.CompareTo(r2._executionTimeInMs);
+            }
+        }
+    }
+
+    public class ResultsTableAdapter : TableDataAdapter
+    {
+        private const int TextSize = 14;
+
+        public ResultsTableAdapter(Context context, IList data)
+            : base(context, data)
+        {
+        }
+
+        public override View GetCellView(int rowIndex, int columnIndex, ViewGroup parentView)
+        {
+            var model = (ResultViewModel)GetRowData(rowIndex);
+            View renderedView = null;
+
+            switch (columnIndex)
+            {
+                case 0:
+                    renderedView = RenderText(model.Method);
+                    break;
+                case 1:
+                    renderedView = RenderText(model.Bytes);
+                    break;
+                case 2:
+                    renderedView = RenderText(model.KBytes);
+                    break;
+                case 3:
+                    renderedView = RenderText(model.SizeDiff);
+                    break;
+                case 4:
+                    renderedView = RenderText(model.TimeAvg);
+                    break;
+            }
+
+            return renderedView;
+        }
+
+        private View RenderText(string text)
+        {
+            var textView = new TextView(Context);
+            textView.Text = text;
+            textView.SetPadding(20, 10, 20, 10);
+            textView.TextSize = TextSize;
+
+            return textView;
+        }
+
+        //private View RenderPrice(Car car)
+        //{
+        //    var textView = new TextView(Context);
+        //    textView.Text = (car.Price / 1000).ToString("R #,##0K");
+        //    textView.SetPadding(20, 10, 20, 10);
+        //    textView.TextSize = TextSize;
+
+        //    if (car.Price < 500000)
+        //    {
+        //        textView.SetTextColor(Color.DarkGreen);
+        //    }
+        //    else if (car.Price > 1000000)
+        //    {
+        //        textView.SetTextColor(Color.DarkRed);
+        //    }
+
+        //    return textView;
+        //}
+
+        //private View RenderPower(Car car, ViewGroup parentView)
+        //{
+        //    var view = LayoutInflater.Inflate(Resource.Layout.TableCellPower, parentView, false);
+        //    var kwView = view.FindViewById<TextView>(Resource.Id.kw_view);
+        //    var psView = view.FindViewById<TextView>(Resource.Id.ps_view);
+
+        //    kwView.Text = car.Kw + " kW";
+        //    psView.Text = car.Ps + " PS";
+
+        //    return view;
+        //}
+
+        //private View RenderName(Car car)
+        //{
+        //    var textView = new TextView(Context);
+        //    textView.Text = car.Name;
+        //    textView.SetPadding(20, 10, 20, 10);
+        //    textView.TextSize = TextSize;
+        //    return textView;
+        //}
+
+        //private View RenderProducerLogo(Car car, ViewGroup parentView)
+        //{
+        //    var view = LayoutInflater.Inflate(Resource.Layout.TableCellImage, parentView, false);
+        //    var imageView = view.FindViewById<ImageView>(Resource.Id.imageView);
+        //    imageView.SetImageResource(car.Producer.Logo);
+        //    return view;
+        //}
+    }
+}
--- AndroidApp/MainActivity.cs
+++ AndroidApp/MainActivity.cs
@@ -5,8 +5,11 @@ using Android.Views;
 using Android.Widget;
 using AndroidApp.Adapters;
 using AndroidX.AppCompat.App;
+using AndroidX.Core.Content;
 using AndroidX.RecyclerView.Widget;
 using ByteSizeLib;
+using CodeCrafters.TableViews;
+using CodeCrafters.TableViews.Toolkit;
 using Core;
 using Google.Android.Material.BottomNavigation;
 using System;
@@ -20,6 +23,7 @@ namespace AndroidApp
         TextView textMessage;
         private RecyclerView _list;
         private ResultsAdapter _adapter = new ResultsAdapter();
+        private ResultsTableAdapter _tableAdapter;
 
         protected override async void OnCreate(Bundle savedInstanceState)
         {
@@ -30,19 +34,7 @@ namespace AndroidApp
             textMessage = FindViewById<TextView>(Resource.Id.message);
             BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
             navigation.SetOnNavigationItemSelectedListener(this);
-
-            _list = FindViewById<RecyclerView>(Resource.Id.test_results_list);
-            _list.SetAdapter(_adapter);
-            textMessage.Text = "Executing tests...";
-
-            var l = new List<TestResult>()
-            {
-                new TestResult("Test", new ByteSize(1000), 12, 40),
-                new TestResult("Test2", new ByteSize(2000), 12, 40),
-                new TestResult("Badjoras3", new ByteSize(3000), 12, 40),
-            };
-            _adapter.SubmitList(l);
-
+            SetupTable(new List<TestResult>());
 
             var contacts = 1000;
             var repeatTests = 1;
@@ -50,12 +42,40 @@ namespace AndroidApp
             var scheduler = new TestScheduler();
             var result = await scheduler.ExecuteTestsAsync(contacts, repeatTests);
 
-            _adapter.SubmitList(result);
-            
-
+            _tableAdapter.AddAll(result);
             textMessage.Text = "Done!";
-
         }
+
+        private void SetupTable(List<TestResult> results)
+        {
+            var resultTableView = FindViewById<SortableTableView>(Resource.Id.tableView);
+
+            var simpleTableHeaderAdapter = new SimpleTableHeaderAdapter(this, "Method", "Bytes", "KB", "Size Diff", "Time Avg (ms)");
+            simpleTableHeaderAdapter.SetTextColor(ContextCompat.GetColor(this, Resource.Color.table_header_text));
+            resultTableView.HeaderAdapter = simpleTableHeaderAdapter;
+
+            var rowColorEven = ContextCompat.GetColor(this, Resource.Color.table_data_row_even);
+            var rowColorOdd = ContextCompat.GetColor(this, Resource.Color.table_data_row_odd);
+            resultTableView.SetDataRowBackgroundProvider(TableDataRowBackgroundProviders.AlternatingRowColors(rowColorEven, rowColorOdd));
+            resultTableView.HeaderSortStateViewProvider = SortStateViewProviders.BrightArrows();
+
+            resultTableView.SetColumnWeight(0, 2);
+            resultTableView.SetColumnWeight(1, 3);
+            resultTableView.SetColumnWeight(2, 3);
+            resultTableView.SetColumnWeight(3, 2);
+            resultTableView.SetColumnWeight(4, 2);
+
+            resultTableView.SetColumnComparator(0, ResultViewModel.GetBytesComparator());
+            resultTableView.SetColumnComparator(1, ResultViewModel.GetTimeComparator());
+            _tableAdapter = new ResultsTableAdapter(this, ResultViewModel.From(results));
+            resultTableView.DataClick += (sender, e) =>
+            {
+                var result = (ResultViewModel)e.ClickedData;
+                var resultString = $"Tapped {result.Method}";
+                Toast.MakeText(this, resultString, ToastLength.Short).Show();
+            };
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
